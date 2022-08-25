03b7ac7326361b9073e4206bae049d5f57a4a255
Elena Pegoraro <elenapoikilia@gmail.com>
2022-08-25 16:28:23 +0200

    model done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :list, presence: true, uniqueness: true
-  validates :movie, presence: true, uniqueness: true
-  validates :movie_id, uniqueness: { scope: [:list_id] }
+  validates :list, presence: true
+  validates :movie, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
 end
