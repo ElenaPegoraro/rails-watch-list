575a0920f25e9d5680385bb50d714e2e2d61a91e
Elena Pegoraro <elenapoikilia@gmail.com>
2022-08-26 12:59:48 +0200

    routing done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,18 @@
 class ListsController < ApplicationController
-  def list
+  def index
     @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @lists = List.show
+  end
+
+  def create
+    @list = List.new
+    @list = List.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "lists", to: "lists#index"
+  # get "lists", to: "lists#index"
   # root "articles#index"
+  resources :lists do # nest bookmarks
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [:destroy]
 end
