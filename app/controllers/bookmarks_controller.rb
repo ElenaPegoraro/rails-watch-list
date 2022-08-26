81c615e4e66ea1e6a6cf2c078b7e747de0be2653
Elena Pegoraro <elenapoikilia@gmail.com>
2022-08-26 16:39:15 +0200

    controller and view almost working

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+  # before_action :set_restaurant, only: %i[new create]
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    @bookmark.movie = Movie.find(bookmark_params[:movie_id])
+    @bookmark.save
+    redirect_to list_path(params[:list_id])
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+ # set list
+  def set_list
+   @list = List.find(params[:list_id])
+  end
+
+  def dstroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,11 +8,18 @@ class ListsController < ApplicationController
   end
 
   def show
-    @lists = List.show
+    @list = List.find(params[:id])
   end
 
   def create
-    @list = List.new
-    @list = List.save
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
